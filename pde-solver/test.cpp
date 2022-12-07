4172104c7aadcb84dbd7382aa6ab565521c20363
celiatun <ge87loj@mytum.de>
2022-12-07 19:22:52 +0100

    Add Catch2 for testing

---
+++ pde-solver/test.cpp
@@ -0,0 +1,17 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+#include <cstdint>
+//#include "" // ???? main.cpp
+
+//int n = 10;
+//int opt1 = 2;
+//int opt2 = 4;
+
+//TEST_CASE("Test and benchmark pde-solver", "[!benchmark]") {
+//  REQUIRE(pde-solver(n,n,opt1,opt2,'r','s') - known <= tol);  // this would require the matrix operator
+
+//  BENCHMARK("") {
+//        return pde-solver(n,m,opt1,opt2,'f','s');
+//    };
+//}
