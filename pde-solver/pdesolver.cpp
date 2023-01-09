33e505c82db6155255908184a0cb7c4cee5013ce
celiatun <ge87loj@mytum.de>
2023-01-09 22:11:37 +0100

    Refactoring: transfer solver code to pdesolver.cpp

---
+++ pde-solver/pdesolver.cpp
@@ -0,0 +1,26 @@
+#include "matrixutils.h"
+#include <iostream>
+#include <vector>
+
+matrix solve_pde(matrix &f, const matrix &h, int steps = 50, bool show_steps = true, bool show_final = false) {
+  for (int t = 1; t <= steps; ++t) {
+    for (int i = 1; i < f._m.size() - 1; ++i) {
+      for (int j = 1; j < f._m[i].size() - 1; ++j) {
+        f._m[i][j] = (f._m[i - 1][j - 1] + f._m[i - 1][j] + f._m[i][j - 1] + f._m[i][j + 1] +
+                   f._m[i + 1][j] + f._m[i + 1][j + 1] + f._m[i - 1][j + 1] +
+                   f._m[i + 1][j - 1]) /
+                      8 +
+                  h._m[i][j] / 2;
+      }
+    }
+    if (show_steps) {
+      f.print();
+      std::cout << "\n";
+    }
+  }
+  if (show_final) {
+    f.print();
+    std::cout << "\n";
+  }
+  return f;
+}
